3e80e14d0234c1d239cb9d2eb3fae032b1e63ad1
luminarnovin <novinjoseph27@gamil.com>
2024-01-03 11:24:12 +0530

    netfilx final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './Components/Banner';
+import Nav from './Components/Nav';
+import Movierow from './Components/Movierow';
+import requests from './requests';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Nav/>
+
+    <Banner fetchUrl={requests.fetchNetflixOriginals}/>
+
+    <Movierow isPresent={true} title ="Trending" fetchUrl={requests.fetchTrending}></Movierow>
+
+     <Movierow title ="NetflixOriginals" fetchUrl={requests.fetchNetflixOriginals}></Movierow>
+
+     <Movierow title ="Top Rated" fetchUrl={requests.fetchTopRated}></Movierow>
+
+     <Movierow title ="Action Movies" fetchUrl={requests.fetchActionMovies}></Movierow>
+
+     <Movierow title ="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Movierow>
+
+     <Movierow title ="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Movierow>
+
+     <Movierow title ="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Movierow>
+
+     <Movierow title ="Documentaries" fetchUrl={requests.fetchDocumentaries}></Movierow>
     </div>
   );
 }
